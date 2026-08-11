2455a8047375320a80651cca1057e220f22deff9
simonequeiroz <sqb1410@gmail.com>
2026-08-10 22:53:02 -0300

    Aula 02 contrutor

--- Aula02/Carro.java
+++ Aula02/Carro.java
@@ -1,15 +1,33 @@
-//cria classe carro
+// Cria a classe chamada Carro
 
-package  Aula02; // package tem o nome de pasta 
+package aula02; // package é por conta da pasta criada
 
-class Carro { // atributos 
-	// cria uma variavel marca do tipo String
-	String marca;
-	String cor; 
+// Criando construtor
+class Carro {
+// Atributos
+// Cria uma variavel marca do tipo String
+String marca;
+String cor;
 
-	//metodo (ação)
-	void buzinar () {
-		// exibir mensagem no terminal 
-		System.out.println("Buzinando...");
-	}
-} 
+// Criando construtor
+
+public Carro(String marcaescolhida, String corescolhida){
+    // this para me referir a um atributo de dentro da classe
+    this.marca = marcaescolhida;
+    this.cor = corescolhida;
+}
+
+// Metodo (Ação)
+
+void buzinar(){
+    // Exibir mensagem no terminal
+    System.out.println("Bibi");
+}
+    
+}
+
+
+
+
+
+
--- Aula02/Principal.java
+++ Aula02/Principal.java
@@ -1,15 +1,15 @@
-package Aula02;
+package aula02;
 
 public class Principal {
     // metodo principal para executar o codigo
     public static void main(String []args){
 
     // Cria o objeto chamado meuCarro
-    Carro meuCarro = new Carro();
-
+    Carro meuCarro = new Carro("Fiat","vermelho");
+     System.out.println("Carro marca: " + meuCarro.marca + " cor: " + meuCarro.cor);
     // Atribuindo a cor e a marca ao objeto
-    meuCarro.marca = "Fiat";
-    meuCarro.cor = "Vermelho";
+    meuCarro.marca = "Nissan";
+    meuCarro.cor = "azul";
     // Exibindo informações do veiculo
     System.out.println("Carro marca: " + meuCarro.marca + " cor: " + meuCarro.cor);
     meuCarro.buzinar();
@@ -17,5 +17,3 @@ public class Principal {
     }
     
 }
-
-
