efb1db8e1e565fe7d4fd89af5113ab7d0f7d12fc
Simone Queiroz <142627749+simonequeiroz@users.noreply.github.com>
2026-08-10 21:13:26 -0300

    aula 02 exemplo classe

---
+++ Aula02/Carro.java
@@ -0,0 +1,15 @@
+//cria classe carro
+
+package  Aula02 // package tem o nome de pasta 
+
+class Carro { // atributos 
+	// cria uma variavel marca do tipo String
+	string marca;
+	string cor; 
+
+	//metodo (ação)
+	void buzinar () {
+		// exibir mensagem no terminal 
+		System.out.println("Buzinando...");
+	}
+} 
