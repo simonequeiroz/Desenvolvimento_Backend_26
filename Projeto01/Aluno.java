27b6170fdeaafa8561ab0a867d7a5421eaf27220
bruninxsz <brunohsp0306@gmail.com>
2026-08-17 21:58:06 -0300

    feat: inicializa Projeto01 com as classes Aluno e Main

---
+++ Projeto01/Aluno.java
@@ -0,0 +1,28 @@
+package Projeto01
+
+public class Aluno {
+    String nome;
+    int idade;
+    double nota1;
+    double nota2;
+
+    void apresentar() {
+        System.out.println("Meu nome é " + nome);
+        System.out.println("Tenho " + idade + " anos");
+    }
+
+    double calcularMedia() {
+        return (nota1 + nota2) / 2;
+    }
+
+    String verificarSituacao() {
+        double media = calcularMedia();
+        if (media >= 7) {
+            return "Aprovado";
+        } else if (media >= 5) {
+            return "Recuperação";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
